766adf5974371c74c2577de64ea6e3689af13af5
Derek Mahar <derek.mahar@protonmail.com>
2025-10-20 20:40:03 -0400

    Add scripts for HTTPie.

---
+++ httpie/get_exercise_source_file.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Enable strict mode.
+set -euo pipefail
+IFS=$'\n\t'
+
+if [[ $# != 2 ]]
+then
+  echo "Usage: $0 [PASSWORD] [SOURCE_FILE_URL]"
+  exit 1
+fi
+
+password=${1:-}
+source_file_url=${2:-}
+
+# Use HTTPie to retrieve source file after submitting password form.
+http $source_file_url \
+  form-name="form 1" \
+  password="$password" \
+  --download \
+  --follow \
+  --form \
+  --quiet
